309d5018dc551d6d2f23033deacbbb94066be07c
XayahSuSuSu <zds1249475336@gmail.com>
2022-08-02 02:12:32 +0000

    Settings: Add Mod Version to firmware version page
    Change-Id: I907f4151203a9cb0213af5230301333629d57f79

---
+++ src/com/android/settings/deviceinfo/firmwareversion/ModVersionPreferenceController.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2022 Acme
+ * Copyright (C) 2022 YuyukoAOSPMod
+ *
+ * PDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class ModVersionPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "ModVersionPreferenceController";
+
+    private static final String KEY_MOD_BRANCH_PROP = "ro.yuyuko.aospmod.branch";
+
+    private static final String KEY_MOD_BUILD_TYPE_PROP = "ro.yuyuko.aospmod.build.type";
+
+    public ModVersionPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String version = SystemProperties.get(KEY_MOD_BRANCH_PROP, mContext.getString(R.string.unknown)).toUpperCase() + " | " + SystemProperties.get(KEY_MOD_BUILD_TYPE_PROP, mContext.getString(R.string.unknown)).toUpperCase();
+        return version;
+    }
+}
